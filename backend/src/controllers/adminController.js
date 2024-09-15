b8c61933c3eba642a23aff2e2d6492da215148e4
ronalmoctz <ronal@ithou>
2024-09-15 17:50:21 -0600

    login arreglado

---
+++ backend/src/controllers/adminController.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcrypt');
+const { createUser } = require('../service/userService');
+
+const registerUserController = async (req, res) => {
+  const { userName, password, userRol } = req.body;
+
+  try {
+    const hashesPassword = await bcrypt.hash(password, 10);
+    const newUser = await createUser(userName, hashesPassword, userRol);
+
+    res
+      .status(201)
+      .json({ message: 'Usuario creado exitosamente', user: newUser });
+  } catch (error) {
+    console.error('Error al crear el usuario'.error);
+    res
+      .status(500)
+      .json({
+        message: 'Error al crear el usuario',
+        error: error.message || 'Desconocido',
+      });
+  }
+};
+
+module.exports = { registerUserController };
