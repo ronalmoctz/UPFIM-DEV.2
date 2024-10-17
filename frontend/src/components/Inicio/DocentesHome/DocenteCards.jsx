19e758c8bb8218a9260428d0b87b51ee410ea6d0
ronalmoctz <moctz23@outlook.com>
2024-10-17 11:57:39 -0700

    babel agree

--- frontend/src/components/Inicio/DocentesHome/DocenteCards.jsx
+++ frontend/src/components/Inicio/DocentesHome/DocenteCards.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React from 'react';
 
 const DocenteCards = ({ docente }) => {
   return (
     <div className="flex flex-1 flex-col gap-5 rounded-md bg-white p-5 dark:bg-slate-800 dark:text-slate-300">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-5">
-          <img src={docente.img_url} alt={docente.nombre} className="h-14 w-14 rounded-full" />
+          <img
+            src={docente.img_url}
+            alt={docente.nombre}
+            className="h-14 w-14 rounded-full"
+          />
           <div>
             <h1 className="text-xl font-semibold">{`${docente.titulo} ${docente.nombre} ${docente.aPater} ${docente.aMater}`}</h1>
           </div>
@@ -15,23 +19,42 @@ const DocenteCards = ({ docente }) => {
       <div className="border-b pb-2">
         <p className="font-bold">Imparte:</p>
         <div className="flex flex-wrap gap-2">
-          {docente.talleres_impartidos.split(', ').map((taller, index) => (
-            <span key={index} className="font-normal bg-gray-200 px-2 py-1 rounded hover:bg-verde hover:text-white transition-colors">
-              {taller}
-            </span>
-          ))}
-        </div>
-        
-        <p className="font-bold">Tipo:</p>
-        <div className="flex flex-wrap gap-2">
-          {docente.tipos_taller.split(', ').map((tipo, index) => (
-            <span key={index} className="font-normal bg-gray-200 px-2 py-1 rounded hover:bg-verde hover:text-white  transition-colors">
-              {tipo}
-            </span>
-          ))}
+          {docente.talleres_impartidos ? (
+            docente.talleres_impartidos.split(', ').map((taller, index) => (
+              <span
+                key={index}
+                className="font-normal bg-gray-200 px-2 py-1 rounded hover:bg-verde hover:text-white transition-colors"
+              >
+                {taller}
+              </span>
+            ))
+          ) : (
+            <span>No tiene talleres asignados</span>
+          )}
         </div>
 
-        <span className="font-bold flex items-center gap-1">Correo: <span className="font-normal">{docente.correo}</span></span>
+        <p className="font-bold">Tipo:</p>
+        <div className="flex flex-wrap gap-2">
+          {docente.tipos_taller ? (
+            docente.tipos_taller.split(', ').map((tipo, index) => (
+              <span
+                key={index}
+                className="font-normal bg-gray-200 px-2 py-1 rounded hover:bg-verde hover:text-white transition-colors"
+              >
+                {tipo}
+              </span>
+            ))
+          ) : (
+            <span>No tiene tipos de taller asignados</span>
+          )}
+        </div>
+
+        <span className="font-bold flex items-center gap-1">
+          Correo:{' '}
+          <span className="font-normal">
+            {docente.correo ? docente.correo : 'Correo no disponible'}
+          </span>
+        </span>
       </div>
     </div>
   );
