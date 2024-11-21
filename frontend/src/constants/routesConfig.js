6eacc3431bddc21fe5cf4a44b960ff6bf436e7f8
Alberto0511 <betopy0511@gmail.com>
2024-11-21 13:08:47 -0600

    fix routes

--- frontend/src/constants/routesConfig.js
+++ frontend/src/constants/routesConfig.js
@@ -3,7 +3,10 @@ import Calificaciones from '../components/Admin/pages/Calificaciones';
 import Actividades from '../components/Admin/pages/Actividades';
 import Talleres from '../components/Admin/pages/Talleres';
 import Galleria from '../components/Admin/pages/Galleria';
+import Administradores from '../components/Admin/pages/Administradores';
 import Alumnado from '../components/Admin/pages/Alumnos';
+import Docentes from '../components/Admin/pages/Docentes';
+import ChangePassword from '../components/Admin/pages/ChangePassword';
 
 const adminRoutes = {
   admin: [
@@ -12,7 +15,10 @@ const adminRoutes = {
     { path: 'actividades', component: Actividades },
     { path: 'talleres', component: Talleres },
     { path: 'galleria', component: Galleria },
-    { path: 'alumnado', component: Alumnado },
+    { path: 'admins', component: Administradores },
+    { path: 'alumnado', component: Alumnado},
+    { path: 'docentes', component: Docentes },
+    { path: 'changePassword', component: ChangePassword },
   ],
 };
 
